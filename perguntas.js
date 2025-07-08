555e8fd043c25741289c6d5c50f156cfbd047146
joaquim131 <joaquim.souza.silva@escola.pr.gov.br>
2025-07-08 09:19:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Jogos',
+    'O Kratos ja teve filho?',
+    'Sim, dois'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantos mundiais o São Paulo tem?',
+    'Tem 3 e o palmeiras 0'
+)
+
+criaCartao(
+    'Jogos',
+    'Em que ano foi lançado o the lest of us?',
+    'O primeiro em 2013 e o segundo em 2020'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantas champions o Real Madrid tem?',
+    '15'
+)
